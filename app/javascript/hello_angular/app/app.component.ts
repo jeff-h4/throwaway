324b091904a880ce4500260f0f825f3e5a123c69
Jeff H <jeff.h.four@gmail.com>
2018-04-20 13:00:09 -0700

    implemented webpacker gem change to support HTML templates in their own files

--- app/javascript/hello_angular/app/app.component.ts
+++ app/javascript/hello_angular/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import templateString from './app.component.html'
 
+  //template: `<h1>Hello {{name}}</h1>`
 @Component({
   selector: 'hello-angular',
-  template: `<h1>Hello {{name}}</h1>`
+  template: templateString
 })
 export class AppComponent {
   name = 'Angular!';
