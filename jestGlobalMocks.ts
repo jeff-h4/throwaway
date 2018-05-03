7d5d3690eda4e33938e097f29bf6effbd0f8c383
Jeff H <jeff.h.four@gmail.com>
2018-05-03 15:13:43 -0700

    Setup Jest config for Angular

---
+++ jestGlobalMocks.ts
@@ -0,0 +1,41 @@
+// Had to comment out the global variable assignment due to a webpack error:
+//
+//    ERROR in /home/dev/proj/throwaway/jestGlobalMocks.ts
+//   [tsl] ERROR in /home/dev/proj/throwaway/jestGlobalMocks.ts(1,1)
+//   TS2304: Cannot find name 'global'.
+//global['CSS'] = null;
+const mock = () => {
+  let storage = {};
+  return {
+    getItem: key => key in storage ? storage[key] : null,
+    setItem: (key, value) => storage[key] = value || '',
+    removeItem: key => delete storage[key],
+    clear: () => storage = {},
+  };
+};
+
+Object.defineProperty(window, 'localStorage', {value: mock()});
+Object.defineProperty(window, 'sessionStorage', {value: mock()});
+Object.defineProperty(document, 'doctype', {
+  value: '<!DOCTYPE html>'
+});
+Object.defineProperty(window, 'getComputedStyle', {
+  value: () => {
+    return {
+      display: 'none',
+      appearance: ['-webkit-appearance']
+    };
+  }
+});
+/**
+ * ISSUE: https://github.com/angular/material2/issues/7101
+ * Workaround for JSDOM missing transform property
+ */
+Object.defineProperty(document.body.style, 'transform', {
+  value: () => {
+    return {
+      enumerable: true,
+      configurable: true,
+    };
+  },
+});
