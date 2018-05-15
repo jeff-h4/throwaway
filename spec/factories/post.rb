5cd5d511034a4e59133e7cea6f6070769ac12342
Jeff H <jeff.h.four@gmail.com>
2018-05-14 17:08:48 -0700

    added missing changes to Post factory

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   now = Time.now
   factory :post do
     title "1 Butterknife"
-    association :owner, factory: :user
+    association :owner, factory: :user, strategy: :build
     created_at now
     updated_at now
   end
