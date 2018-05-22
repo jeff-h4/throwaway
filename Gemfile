711d52112a874ce9c23b2d7995824223b4ded28b
Jeff H <jeff.h.four@gmail.com>
2018-05-22 10:38:35 -0700

    Setup Rubocop

--- Gemfile
+++ Gemfile
@@ -63,6 +63,11 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'rubocop', '~> 0.56.0'
+  gem 'ruby-lint'
+  gem 'reek'
+  gem 'fasterer'
+  gem 'debride'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  describe "GET #index" do
+  describe 'GET #index' do
     subject { get :index }
 
-    describe "when request is unauthenticated" do
-      include_examples "controller rejects unauthenticated requests"
+    describe 'when request is unauthenticated' do
+      include_examples 'controller rejects unauthenticated requests'
     end
 
-    describe "when request is authenticated", :logged_in do
-      include_examples "controller responds with correct status code", 200
+    describe 'when request is authenticated', :logged_in do
+      include_examples 'controller responds with correct status code', 200
     end
   end
 end
